01f5fbf7322d8da50438a234459bd7c58e48d737
Amaan Sayyed <amaansayyed1@gmail.com>
2026-02-24 22:42:09 +0530

    update feb 24

--- C/codes/sample.c
+++ C/codes/sample.c
@@ -1,23 +1,3 @@
-/// file  to check codes 
+///ignore 
+//file to check codes 
 
-
-
-#include <stdio.h>
-int main(){
-int arr[5];
-int i;
-
-for ( i = 0; i <5 ; i++)
-{
-
-printf("enter a number ");
-scanf("%d", );
-
-
-}
- 
-
-
-
-
-}
