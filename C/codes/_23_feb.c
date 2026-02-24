2b98ebf97fd0efbfd12664f97c551c0404ce3ce1
Amaan Sayyed <amaansayyed1@gmail.com>
2026-02-24 22:42:09 +0530

    update feb 24

--- C/codes/_23_feb.c
+++ C/codes/_23_feb.c
@@ -195,26 +195,37 @@ for(int i=1;i<=5;i++ ){
 // *****
 
 
-#include<stdio.h>
-int main(){
-int  i , j;
-int spnt = 4 ;
-for(i = 1; i<= 5;i++){
+#include <stdio.h>
 
-for(j = 1 ; j <= spnt; j++){
-  
-}
-for(j = 1 ; j<= i ; j ++ ){
-    printf("*");
-    spnt--;
-    
-}
-printf("  ");
-}
+int main() {
 
-return 0 ;
+    int i, j;
+    int spnt = 4;
+
+    for (i = 1; i <= 5; i++) {
+
+        // Print spaces
+        for (j = 1; j <= spnt; j++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+
+        spnt--;   // decrease after each row
+    }
+
+    return 0;
 }
-// *  **  ***  ****  *****
+//     *
+//    **
+//   ***
+//  ****
+// *****
  
 // 78  ...AREA AND VOLUME OF CUBOID 
 
--- C/codes/_24_feb.c
+++ C/codes/_24_feb.c
@@ -35,3 +35,199 @@ void cal(int a,int b ,int*add,int*sub,int*mul){
 
 
 
+#include <stdio.h>
+
+int main() {
+
+    int arr[5];
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("\narr[%d] = %d", i, arr[i]);
+
+        printf("\n*(arr + %d) = %d", i, *(arr + i));
+
+        printf("\n%d[arr] = %d", i, i[arr]);
+
+        printf("\n-------------------");
+    }
+
+    return 0;
+}
+// Enter a number: 1
+// Enter a number: 2
+// Enter a number: 3
+// Enter a number: 4
+// Enter a number: 5
+
+// arr[0] = 1
+// *(arr + 0) = 1
+// 0[arr] = 1
+// -------------------
+// arr[1] = 2
+// *(arr + 1) = 2
+// 1[arr] = 2
+// -------------------
+// arr[2] = 3
+// *(arr + 2) = 3
+// 2[arr] = 3
+// -------------------
+// arr[3] = 4
+// *(arr + 3) = 4
+// 3[arr] = 4
+// -------------------
+// arr[4] = 5
+// *(arr + 4) = 5
+// 4[arr] = 5
+// -------------------
+
+
+// ARRAY with fun EXAMPLES
+#include <stdio.h>
+void dis_arr(int *ptr) ;
+int main() {
+
+    int arr[5];
+    int i;
+printf("Enter number to array: ");
+    for (i = 0; i < 5; i++) {
+        
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Base address of array = %p\n", arr);
+
+    dis_arr(arr);  
+
+    return 0;
+}
+
+void dis_arr(int *ptr) {
+
+    int i;
+
+    printf("\nFirst element using pointer = %d\n", *ptr);
+
+    for (i = 0; i < 5; i++) {
+        printf("ptr[%d] = %d\n", i, ptr[i]);
+    }
+}
+// Enter number to array: 1 2 3 4 5
+// Base address of array = 0061FF08
+
+// First element using pointer = 1
+// ptr[0] = 1
+// ptr[1] = 2
+// ptr[2] = 3
+// ptr[3] = 4
+// ptr[4] = 5
+
+//........... 2D  ARRAYS
+
+// DEMO FOR 2D ARRAY :
+#include <stdio.h>
+
+void dis_arr1(int arr[][3]);
+
+int main()
+{
+    int arr[3][3], i, j;
+
+    printf("Enter values for 3x3 array:\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    dis_arr(arr);
+
+    return 0;
+}
+
+void dis_arr1(int arr[][3])
+{
+    int i, j;
+
+    printf("\nMatrix elements are:\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+// Enter values for 3x3 array:
+// 1 2 3 4 5 6 7 8 9 
+
+// Matrix elements are:
+// 1 2 3 
+// 4 5 6 
+// 7 8 9 
+
+//.........ADD 2 2D ARRAY
+#include <stdio.h>
+
+void display_arr(int arr[3][3]);
+void accept_arr(int arr[3][3]);
+int main()
+{
+    
+int a[3][3],  b[3][3],  sum[3][3],i,j;
+
+    accept_arr(a);
+    accept_arr(b);
+   for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    display_arr(sum);
+
+    return 0;
+}
+
+void display_arr(int arr[3][3])
+{
+    int i, j;
+
+    printf("\n Sum of 2 2D array are:\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void accept_arr(int arr[3][3]){
+int i, j;
+    printf("Enter values for 3x3 array:\n");
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+        scanf("%d", &arr[i][j]);
+        }
+    }
+
+}
+// Enter values for 3x3 array:
+// 1 2 3 4 5 6 7 8 9
+// Enter values for 3x3 array:
+// 11 22 33 44 55 66 77 88 99
+
+//  Sum of 2 2D array are:
+// 12 24 36
+// 48 60 72
+// 84 96 108
