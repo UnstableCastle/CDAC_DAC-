5ae64e61b9c8fde167846e32133be49b39c640e6
Amaan Sayyed <amaansayyed1@gmail.com>
2026-03-05 20:52:34 +0530

    sql + c++ lab

--- C++/code/_04_mar.cpp
+++ C++/code/_04_mar.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Date{
 int dd,mm,yy;
 
+
 public:
 
 void set_Date(int d,int m, int y){
@@ -78,3 +79,34 @@ d1.display();
 
 // pointer(x,y)
 
+#include <iostream>
+using namespace std;
+
+class values {
+    int x, y;
+
+public:
+
+    void set_values(int d, int m) {
+        this->x = d;
+        this->y = m;
+    cout<<"\n";    
+        cout<<"address 0f x \n"<<&x;
+        cout<<"\n";
+           cout<<"address 0f Y \n"<<&y;
+    }
+
+    void display() {
+        cout << "Values = X" << this->x << "\tY = "<< this->y;
+    }
+};
+
+int main() {
+    values d1;
+    d1.set_values(1, 12);
+    cout<<"address 0f d1 \n"<<&d1;
+    cout<<"\n";
+ 
+    cout<<"\n";
+    d1.display();
+}
