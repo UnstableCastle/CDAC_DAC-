5996d7eab181a32c590331fcd1c6b201ebffe875
Amaan Sayyed <amaansayyed1@gmail.com>
2026-03-06 20:57:27 +0530

    6-3-26

---
+++ C++/code/_06_mar.cpp
@@ -0,0 +1,34 @@
+//operator overloading
+
+#include<iostream>
+
+using namespace std;
+
+class MyComplex{
+int real , img;
+
+public : 
+
+MyComplex(){
+    real = 0;
+    img= 0;
+    
+}
+
+
+
+
+
+
+
+};
+
+
+
+
+//copy constructer 
+
+
+
+
+
