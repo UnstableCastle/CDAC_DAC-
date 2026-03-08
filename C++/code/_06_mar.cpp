afc1c7a620297cb35fd025bcb6dd61d295efd455
Amaan Sayyed <amaansayyed1@gmail.com>
2026-03-09 00:30:42 +0530

    9-3-26

--- C++/code/_06_mar.cpp
+++ C++/code/_06_mar.cpp
@@ -1,7 +1,6 @@
 //operator overloading
 
 #include<iostream>
-
 using namespace std;
 
 class MyComplex{
@@ -10,40 +9,59 @@ int real , img;
 public : 
 
 MyComplex(){
-cout<<"default";
-
+    cout<<"default\n";
     real = 0;
-    img= 0;
-     
-
+    img = 0;
 }
 
 MyComplex(int r ,int i){
-cout<<"para";
-real = r;
-img = i;
-
+    cout<<"para\n";
+    real = r;
+    img = i;
 }
 
 ~MyComplex(){
-cout<<"destructor";
-
-
+    cout<<"destructor\n";
 }
 
-void set_Value(){
+//member functions
+void set_Real(int r){
+    this->real = r;
+}
 
+void set_Complex(int i){
+    this->img = i;
+}
+
+MyComplex Add(MyComplex c){
+    MyComplex temp;
+    temp.real = this->real + c.real;
+    temp.img = this->img + c.img;
+    return temp;
+}
+
+MyComplex sub(MyComplex c){
+    MyComplex temp;
+    temp.real = this->real - c.real;
+    temp.img = this->img - c.img;
+    return temp;
+}
+
+void display(){
+    cout<<"Number = "<<real<<" + "<<img<<"i"<<endl;
+}
+
+}; // end class
+
+int main(){
+
+    MyComplex c1(1,3), c2(2,3);
     
+    MyComplex c3 = c1.Add(c2);   // store result
+    
+    c3.display();
+
 }
-
-
-
-
-
-};// end of class
-
-
-
 
 //copy constructer 
 
