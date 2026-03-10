adc01e0ff067d4d6e2fae8bd6396c6510d48ad7c
Amaan Sayyed <amaansayyed1@gmail.com>
2026-03-10 20:30:32 +0530

    10-3-26

--- C++/code/_09_mar.cpp
+++ C++/code/_09_mar.cpp
@@ -76,3 +76,63 @@ return 0 ;
 
 
 }
+
+#include <iostream>
+using namespace std;
+
+class Shape{
+protected:
+    int para;
+
+public:
+
+    Shape(){}
+
+    Shape(int n){
+        para = n;
+    }
+
+    void accept_number(){
+        cout<<"Enter para : \n";
+        cin>>para;
+    }
+};
+
+class Circle : public Shape{
+
+public:
+
+    double calArea(){
+        return 3.14 * para * para;
+    }
+
+    void display(){
+        cout<<"Area of Circle = \n"<<calArea()<<"\n";
+    }
+};
+
+class Square : public Shape{
+
+public:
+
+    double calArea(){
+        return para * para;
+    }
+
+    void display(){
+        cout<<"Area of Square = \n"<<calArea()<<"\n";
+    }
+};
+
+int main(){
+
+    Circle c1;
+    Square s1;
+
+    c1.accept_number();
+    c1.display();
+
+    s1.accept_number();
+    s1.display();
+
+}
