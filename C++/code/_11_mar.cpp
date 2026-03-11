5f3accc1fadd4446e329bda45d8341fb2d7b925d
Amaan Sayyed <amaansayyed1@gmail.com>
2026-03-11 17:36:54 +0530

    11-3-26

---
+++ C++/code/_11_mar.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+class ABC
+{
+    private:
+    	static int ref;
+        static ABC* self;
+        ABC()
+        {
+        	cout<<"\n constructor.."<<endl;
+        }
+
+    public:
+        static ABC* getInstance();
+        static int getref();
+
+    };
+int ABC::getref()
+{
+	return ref;
+}
+
+ABC* ABC::getInstance()
+{
+    if (self == 0)
+    {
+      //  ref=1;
+		self = new ABC();
+    }
+    ref=ref+1;
+    return self;
+}
+
+
+ABC* ABC::self;
+int ABC::ref=0;
+int main()
+{
+    //ABC obj;
+    ABC* ptr = ABC::getInstance();
+    ABC* sptr = ABC::getInstance();
+    ABC* pptr = ABC::getInstance();
+     
+    std::cout << ptr << std::endl;
+    std::cout << sptr << std::endl;
+    std::cout << ABC::getref();
+}
+
+
+//
+
