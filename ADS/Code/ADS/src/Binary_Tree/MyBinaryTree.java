78e07478e640f8dc11ec369158d4d71be13f11da
Amaan Sayyed <amaansayyed1@gmail.com>
2026-04-30 16:04:47 +0530

    30-4-26

--- ADS/Code/ADS/src/Binary_Tree/MyBinaryTree.java
+++ ADS/Code/ADS/src/Binary_Tree/MyBinaryTree.java
@@ -1,5 +1,11 @@
 package Binary_Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import LinkedList.MyLinkedList;
+import LinkedList.QueueLL;
+
 class BinaryTree {
 	private BTNode root;
 
@@ -44,7 +50,7 @@ class BinaryTree {
 	public void inorder() {
 		System.out.print("In-order:   ");
 		visitInOrder(root);
-		System.out.println(" (Notice this is sorted!)");
+
 	}
 
 	private void visitPostOrder(BTNode r) {
@@ -56,7 +62,7 @@ class BinaryTree {
 	}
 
 	public void postorder() {
-		System.out.print("Post-order: ");
+		System.out.print("\nPost-order: ");
 		visitPostOrder(root);
 		System.out.println();
 	}
@@ -68,14 +74,44 @@ class BinaryTree {
 	
 	public void display_by_level() {
 		if(root==null) {
-			System.out.println("");
+			System.out.println("Empty !!!");
+			return;
 		}
+		Queue<BTNode> q = new LinkedList<BTNode>();
+		BTNode iter ;
+		q.add(root);
+		System.out.print("\nDisplay_by_Level: ");
+		while(!q.isEmpty()) {
+			iter=q.remove();
+			System.out.print(iter.getData()+" ");
+			if(iter.getLeft()!=null)q.add(iter.getLeft());
+			if(iter.getRight()!=null)q.add(iter.getRight());
+		
+		}		
+		
 	}
 	
-	
-	
-	
-	
+	public void display_by_level_leaf() {
+		if(root==null) {
+			System.out.println("Empty !!!");
+			return;
+		}
+		Queue<BTNode> q = new LinkedList<BTNode>();
+		BTNode iter ;
+		q.add(root);
+		System.out.print("\ndisplay_by_level_leaf: ");
+		while(!q.isEmpty()) {
+			iter=q.remove();
+			if(iter.getLeft()==null && iter.getRight()==null) {
+				System.out.print(iter.getData()+" ");	
+			}
+			
+			if(iter.getLeft()!=null)q.add(iter.getLeft());
+			if(iter.getRight()!=null)q.add(iter.getRight());
+		
+		}		
+		
+	}
 	
 	
 	
@@ -88,26 +124,28 @@ class BinaryTree {
 public class MyBinaryTree {
 	public static void main(String[] args) {
 		BinaryTree bt = new BinaryTree();
-//
-//		bt.setRoot(bt.createNode(50));
-//		bt.getRoot().setLeft(bt.createNode(17));
-//		bt.getRoot().setRight(bt.createNode(72));
-//
-//		bt.getRoot().getLeft().setLeft(bt.createNode(12));
-//		bt.getRoot().getLeft().setRight(bt.createNode(23));
-//
-//		bt.getRoot().getRight().setLeft(bt.createNode(54));
-//		bt.getRoot().getRight().setRight(bt.createNode(76));
-//
-//		bt.getRoot().getLeft().getLeft().setLeft(bt.createNode(9));
-//		bt.getRoot().getLeft().getLeft().setRight(bt.createNode(14));
-//
-//		bt.getRoot().getLeft().getRight().setLeft(bt.createNode(19));
-//		bt.getRoot().getRight().getLeft().setRight(bt.createNode(67));
-//
-//		System.out.println("");
-//		bt.preorder();
-//		bt.inorder();
-//		bt.postorder();
+
+		bt.setRoot(bt.createNode(50));
+		bt.getRoot().setLeft(bt.createNode(17));
+		bt.getRoot().setRight(bt.createNode(72));
+
+		bt.getRoot().getLeft().setLeft(bt.createNode(12));
+		bt.getRoot().getLeft().setRight(bt.createNode(23));
+
+		bt.getRoot().getRight().setLeft(bt.createNode(54));
+		bt.getRoot().getRight().setRight(bt.createNode(76));
+
+		bt.getRoot().getLeft().getLeft().setLeft(bt.createNode(9));
+		bt.getRoot().getLeft().getLeft().setRight(bt.createNode(14));
+
+		bt.getRoot().getLeft().getRight().setLeft(bt.createNode(19));
+		bt.getRoot().getRight().getLeft().setRight(bt.createNode(67));
+
+		System.out.println("");
+		bt.preorder();
+		bt.inorder();
+		bt.postorder();
+		bt.display_by_level();
+		bt.display_by_level_leaf();
 	}
 }
