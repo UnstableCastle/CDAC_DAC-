b35617f86622b7bfdc7761f31d4324306c873f0b
Amaan Sayyed <amaansayyed1@gmail.com>
2026-05-12 20:53:47 +0530

    12-5-26

---
+++ LeetCode_Solutions/Code/Binary Tree/Same Tree.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if( p == null && q == null ){return true;}
+        if(p == null || q == null){return false;}
+
+        Queue<TreeNode> q1 = new LinkedList<>();
+         Queue<TreeNode> q2 = new LinkedList<>();
+         q1.add(p);
+        q2.add(q);
+         while(!q1.isEmpty()&& !q2.isEmpty()){
+            TreeNode n1 = q1.remove();
+            TreeNode n2 = q2.remove();
+
+            if(n1.val != n2.val){return false;}
+            if(n1.left != null && n2.left !=null){
+                q1.add(n1.left);
+                q2.add(n2.left);
+
+            }else if(n1.left != null || n2.left != null){
+                return false;
+            }
+
+             if(n1.right != null && n2.right !=null){
+                q1.add(n1.right);
+                q2.add(n2.right);
+
+            }else if(n1.right != null || n2.right != null){
+                return false;
+            }
+         }
+         return q1.isEmpty()&& q2.isEmpty();
+    }
+
+}
+
+    
+    
---
+++ LeetCode_Solutions/SQL/sol.sql
@@ -0,0 +1,21 @@
+-- combine two tables
+SELECT p.firstname,p.lastname,a.city,a.state from Person p
+Left join Address a on p.personId = a.personId;
+
+-- Second highest salary
+SELECT p.firstname,p.lastname,a.city,a.state from Person p 
+Left join Address a on p.personId = a.personId;
+
+-- nth highest salary
+  CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+ SET N = N - 1;
+    RETURN (
+         # Write your MySQL query statement below.
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT N, 1
+    );
+END
+
