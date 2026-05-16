0911f65ad20823e2ae5dd6453142cf86d0e64c3a
Amaan Sayyed <amaansayyed1@gmail.com>
2026-05-16 21:19:03 +0530

    16-5-26

--- LeetCode_Solutions/SQL/sol.sql
+++ LeetCode_Solutions/SQL/sol.sql
@@ -22,3 +22,6 @@ END
 --584. Find Customer Referee
 SELECT name FROM Customer WHERE referee_id IS NULL OR referee_id <> 2;
 
+--1280. Students and Examinations
+SELECT  s.student_id,s.student_name,sub.subject_name,COUNT(e.subject_name) AS attended_exams FROM Students s JOIN Subjects sub LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND sub.subject_name = e.subject_name GROUP BY s.student_id, s.student_name, sub.subject_name ORDER BY s.student_id,sub.subject_name;
