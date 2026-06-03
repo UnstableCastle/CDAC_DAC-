887721faa3968f9dd7a8d9db07c95cca477611e7
Amaan Sayyed <amaansayyed1@gmail.com>
2026-06-03 17:53:15 +0530

    3-6-26

---
+++ Adv_Java/SpringBoot/Spring_Web_Security/src/main/java/com/app/ThymeleafCrudDemo2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCrudDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafCrudDemo2Application.class, args);
+	}
+
+}
