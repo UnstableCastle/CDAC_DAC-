2b2b1cfef70c402dca705efd372ad0bf668db7ed
Amaan Sayyed <amaansayyed1@gmail.com>
2026-06-04 18:05:59 +0530

    4-6-26

--- Adv_Java/SpringBoot/JWT_Demo/src/main/java/com/app/config/SecurityConfig.java
+++ Adv_Java/SpringBoot/JWT_Demo/src/main/java/com/app/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +26,7 @@ public class SecurityConfig {
 		http.csrf(customizer -> customizer.disable());
 		http.authorizeHttpRequests(
 				request -> request.requestMatchers("/register", "/login").permitAll().anyRequest().authenticated());
-		//http.formLogin(Customizer.withDefaults());
+		// http.formLogin(Customizer.withDefaults());
 		http.httpBasic(Customizer.withDefaults());
 		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
@@ -45,5 +47,11 @@ public class SecurityConfig {
 
 	@Autowired
 	UserDetailsService userDetailsService;
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+
+	}
 
 }
--- Adv_Java/SpringBoot/JWT_Demo/src/main/java/com/app/controller/UserController.java
+++ Adv_Java/SpringBoot/JWT_Demo/src/main/java/com/app/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.app.controller;
 
+import  org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class UserController {
 
 	private BCryptPasswordEncoder byBcryptPasswordEncoder = new BCryptPasswordEncoder(12);
 
-	private BCryptPasswordEncoder byBCryptPasswordEncoder2 = new BCryptPasswordEncoder(BCryptVersion.$2A, 12);
+//	private BCryptPasswordEncoder byBCryptPasswordEncoder2 = new BCryptPasswordEncoder(BCryptVersion.$2A, 12);
 
 	@PostMapping("register")
 	public User register(@RequestBody User user) {
@@ -28,5 +30,22 @@ public class UserController {
 		System.out.println("Password : " + user.getPassword());
 		return userService.saveUser(user);
 	}
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	
+	@PostMapping("/login")
+	public String login(@RequestBody User user) {
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername() ,user.getPassword()));
+
+		if(authentication.isAuthenticated()) {
+			return "Success!!!";
+		}
+		else {
+			return "Failed";
+		}
+	}
+	
 
 }
