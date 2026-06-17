cedfe3f917595e1fc856b844e1501588fcea4c7c
Amaan Sayyed <amaansayyed1@gmail.com>
2026-06-17 17:15:16 +0530

    17-6-26

--- DOT-Net/Assignements/Question11/Program.cs
+++ DOT-Net/Assignements/Question11/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 10;
+            int y = 20;
+            int z = 30;
+
+            if ((x > y) && (x > z))
+            {
+                Console.WriteLine($"Max = {x}");
+            }
+            else if ((y > x) && (y > z))
+            {
+                Console.WriteLine($"Max = {y}");
+            }
+            else
+            {
+                Console.WriteLine($"Max = {z}");
+            }
+
+
+            int max = (x > y) ? ((x > z) ? x : z): ((y > z) ? y : z);
+
+            Console.WriteLine($"Max = {max}");
+
         }
-    }
+}
 }
--- DOT-Net/Assignements/Question11/obj/Debug/net10.0/Question11.AssemblyInfo.cs
+++ DOT-Net/Assignements/Question11/obj/Debug/net10.0/Question11.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Question11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92646fe955b3e763732c0e3f9db42caadf3bffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a47157a2766bfcfd4df260cf528a3deb2f86804")]
 [assembly: System.Reflection.AssemblyProductAttribute("Question11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Question11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
