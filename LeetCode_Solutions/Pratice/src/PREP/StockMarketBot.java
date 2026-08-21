272e9c2bb860b64051cced18b445b729ea464b02
Amaan Sayyed <amaansayyed1@gmail.com>
2026-08-21 18:09:55 +0530

    prep

--- LeetCode_Solutions/Pratice/src/PREP/StockMarketBot.java
+++ LeetCode_Solutions/Pratice/src/PREP/StockMarketBot.java
@@ -3,14 +3,34 @@ import java.util.*;
 public class StockMarketBot {
 public static void main(String[] args) {
 	
+	Scanner sc = new Scanner(System.in);
 	
+	int n = sc.nextInt();
+	int[] prices = new int[n];
 	
+	for(int i=0;i<n;i++) {
+		prices[i] = sc.nextInt();
+	}
 	
+	int lowestPrice = Integer.MAX_VALUE;
+	int maxProfit =0;
 	
+	for(int i = 0;i<n;i++) {
+		int currentPrice = prices[i];
+		
+	if(currentPrice<lowestPrice) {
+		lowestPrice=currentPrice;
+	}
+	else {
+		int profit = currentPrice-lowestPrice;
+		if(profit > maxProfit) {
+			maxProfit = profit;
+		}
+	}
 	
-	
-	
-	
-	
+	}
+	System.out.println(maxProfit);
+	sc.close();
+		
 }
 }
