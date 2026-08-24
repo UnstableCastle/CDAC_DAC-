a0923c7c70e2468a600d3d7670a5e76aaa48dc8e
Amaan Sayyed <amaansayyed1@gmail.com>
2026-08-24 17:43:09 +0530

    PREP

---
+++ LeetCode_Solutions/Pratice/src/Core/MyChef.java
@@ -0,0 +1,15 @@
+package Core;
+
+public class MyChef extends Thread{
+
+	@Override
+	public void run() {
+		System.out.println("chef is cokking on the thread "+Thread.currentThread().getId());
+	}
+	public static void main(String[] args) {
+		
+		MyChef chef = new MyChef();
+		chef.start();
+		
+	}
+}
